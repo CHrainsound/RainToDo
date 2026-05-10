e6da4094cb96f5a4d8153caa9498a5c6dcdd9773
SoRC <3014386984@qq.com>
2026-05-10 12:26:18 +0800

    :art: 改为kotlin

--- app/src/main/java/com/example/raintodo/view/adapter/ViewPager2Adapter.kt
+++ app/src/main/java/com/example/raintodo/view/adapter/ViewPager2Adapter.kt
@@ -1,33 +1,22 @@
-package com.example.raintodo.view.adapter;
+package com.example.raintodo.view.adapter
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.raintodo.view.fragment.MainFragment
+import com.example.raintodo.view.fragment.UserFragment
 
-import com.example.raintodo.view.fragment.MainFragment;
-import com.example.raintodo.view.fragment.UserFragment;
-
-public class ViewPager2Adapter extends FragmentStateAdapter {
-    public ViewPager2Adapter(@NonNull FragmentActivity fragmentActivity) {
-        super(fragmentActivity);
-    }
-
-    @NonNull
-    @Override
-    public Fragment createFragment(int position) {
-        switch(position) {
-            case 0:
-                return new MainFragment();
-            case 1:
-                return new UserFragment();
-            default:
-                return new MainFragment();
+class ViewPager2Adapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
+    override fun createFragment(position: Int): Fragment {
+        when (position) {
+            0 -> return MainFragment()
+            1 -> return UserFragment()
+            else -> return MainFragment()
         }
     }
 
-    @Override
-    public int getItemCount() {
-        return 2;
+    override fun getItemCount(): Int {
+        return 2
     }
 }
